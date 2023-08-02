0d5dafde6d3f1f9eeaccb1c3aa597f8321040184
Davidps9 <david.pujol.santander@gmail.com>
2023-08-02 19:08:35 +0200

    added package

--- Assets/Scripts/EnemySpawScript.cs
+++ Assets/Scripts/EnemySpawScript.cs
@@ -4,25 +4,30 @@ using UnityEngine;
 
 public class EnemySpawScript : MonoBehaviour
 {
-    [SerializeField] EnemyClass dragons;
-    [SerializeField] private float spawnRadius = 10f;
+    [SerializeField] EnemyClass[] enemys;
+    [SerializeField] private float spawnRadius = 300f;
     [SerializeField] GameObject enemyParent;
     [SerializeField] private Biome LocalBiome;
     // Start is called before the first frame update
     void Start()
     {
-        foreach (Enemy enemy in dragons.enemys)
+        foreach (EnemyClass enemyClass in enemys)
         {
-           foreach(Biome biome in enemy.biomes)
+            foreach (Enemy enemy in enemyClass.enemys)
             {
-                if (LocalBiome == biome)
+                foreach (Biome biome in enemy.biomes)
                 {
-                    GameObject parent = Instantiate(enemyParent);
-                    GameObject enemyInstantiation = Instantiate(enemy.model_prefab);
-                    enemyInstantiation.transform.parent = parent.transform;
-                    enemyParent.transform.position = new Vector3(Random.Range(-spawnRadius, spawnRadius), 0, Random.Range(-spawnRadius, spawnRadius));
+                    if (LocalBiome == biome)
+                    {
+                        Vector3 spawnpos = new Vector3(Random.Range(-spawnRadius, spawnRadius), 0, Random.Range(-spawnRadius, spawnRadius));
+                        GameObject parent = Instantiate(enemyParent, spawnpos, Quaternion.identity);
+                        GameObject enemyInstantiation = Instantiate(enemy.model_prefab, spawnpos, Quaternion.identity);
+                        enemyInstantiation.transform.parent = parent.transform;
+                        enemyParent.name = enemy.name;
+                    }
                 }
             }
+         
 
         }
     }
